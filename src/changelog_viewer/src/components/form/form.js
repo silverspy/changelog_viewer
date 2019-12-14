b7d7662a867fa76157f9a4a153d14b147c29bbe0
Emma Dos Santos <emmados@orange.fr>
2019-12-14 20:14:16 +0100

    Add show commit for GitLab URL

--- src/changelog_viewer/src/components/form/form.js
+++ src/changelog_viewer/src/components/form/form.js
@@ -8,22 +8,32 @@ import List from '../list/list';
 class Form extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { dataGit: "" };
+        this.state = { dataGit: "", git: ""};
         this.getCommitsMessage = this.getCommitsMessage.bind(this);
     }
 
     // Function to get all commits messages from a git
     getCommitsMessage() {
         // construct the url
-        var url = document.getElementById('urlGit').value.replace('https://github.com/', 'https://api.github.com/repos/');
-        url = url + "/commits";
+        var url = document.getElementById('urlGit').value;
+        var gitName = "";
+        if (url.includes("github.com")){
+            url = url.replace('https://github.com/', 'https://api.github.com/repos/');
+            url = url + "/commits";
+            gitName = "github";
+        } else if (url.includes("gitlab.com")){
+            url = "https://gitlab.com/api/v4/projects/15874935/repository/commits";
+            gitName = "gitlab";
+        }
 
         // Get request to get datas
         axios.get(url)
             // success : set the state to store commits messages
             .then((data) => {
+                console.log(data);
                 this.setState({
-                    dataGit: data.data
+                    dataGit: data.data,
+                    git: gitName
                 });
             })
             // catch the error
